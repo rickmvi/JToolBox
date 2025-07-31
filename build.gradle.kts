8f5ea781756b6fb92cf559e9a7eaff4985c3f1d3
rickmvi <rickmviana.dev@outlook.com>
2025-07-30 23:35:16 -0300

    feat: added license

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.rickmvi"
-version = "1.1.2"
+version = "1.2.2"
 
 repositories {
     mavenCentral()
--- src/main/java/com/github/rickmvi/jtoolbox/control/Conditionals.java
+++ src/main/java/com/github/rickmvi/jtoolbox/control/Conditionals.java
@@ -1,3 +1,20 @@
+/*
+ * Console API - Utilitarian library for input, output and formatting on the console.
+ * Copyright (C) 2025  Rick M. Viana
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.github.rickmvi.jtoolbox.control;
 
 import org.jetbrains.annotations.Contract;
